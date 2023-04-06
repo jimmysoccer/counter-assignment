5cbba7b08ba4a7f87c5ce47da4435c07c467d976
Sunheng <jimmysoccer0927@gmail.com>
2023-04-06 15:14:24 -0400

    modify counter test

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,41 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Counter from "../components/Counter";
+
+let counter;
+let incrementCount;
+let decrementCount;
 
 beforeEach(() => {
   // Render the Counter component here
-})
-
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  // eslint-disable-next-line testing-library/no-render-in-setup
+  render(<Counter></Counter>);
+  counter = screen.getByTestId("count");
+  incrementCount = screen.getByRole("button", { name: "+" });
+  decrementCount = screen.getByRole("button", { name: "-" });
 });
 
-test('should render initial count with value of 0', () => {
+test("renders counter message", () => {
   // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText("Counter");
+  expect(counterMessage).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
+test("should render initial count with value of 0", () => {
   // Complete the unit test below based on the objective in the line above
+  expect(counter).toHaveTextContent("0");
 });
 
-test('clicking - decrements the count', () => {
+test("clicking + increments the count", () => {
   // Complete the unit test below based on the objective in the line above
+  userEvent.click(incrementCount);
+  expect(counter).toHaveTextContent("1");
+});
+
+test("clicking - decrements the count", () => {
+  // Complete the unit test below based on the objective in the line above
+  userEvent.click(decrementCount);
+  expect(counter).toHaveTextContent("-1");
 });
